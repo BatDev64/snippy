c832d7b3100582b6da5cb64b09fa49c16943c7bd
BatDev64 <bat.bits64@gmail.com>
2025-08-19 05:37:05 -0300

    feat: redesign layout and update heading styles

--- src/app/(overview)/page.tsx
+++ src/app/(overview)/page.tsx
@@ -1,51 +1,28 @@
 import { getAnonURLsByClientId } from "@/lib/data";
 import { Form } from "@/components/anon/CreateURL";
 import { Suspense } from "react";
-import { ListURLs } from "@/components/anon/ListURLs";
+import { ShortLinkList } from "@/components/anon/ShortLinkList";
 
 export default async function Home() {
   const urls = await getAnonURLsByClientId()
-
+  /* */
   return (
-    <>
-      <header className="lg:max-w-md flex flex-col gap-8">
-        <div className="text-balance [&_h1]:leading-[1.1111111] [&_h1]:mb-6 [&_h2]:text-balance [&_h2]:text-sm/7 md:[&_h2]:text-base/7 ">
-          <h1 className="text-5xl md:text-6xl dark:text-neutral-50">
-            Turn endless URLs into flawless links
-          </h1>
-          <h2 className="dark:text-neutral-300">
-            <b>No account required</b>. Up to 10 links per session, each <b>active for 30 minutes...</b>
-          </h2>
-        </div>
-
-        {/* 👉 Mobile: sticky | Desktop: static */}
-        <div className="sticky top-8 z-50 bg-amber-300 lg:static">
+    <div className="w-full mx-auto px-2 md:px-4 md:max-w-7xl my-4">
+      <div className="flex flex-col justify-center items-center gap-12">
+        <header className="text-balance text-center flex flex-col justify-center items-center gap-10">
+          <div className="sm:max-w-lg md:max-w-xl">
+            <h1 className="text-4xl xs:text-5xl sm:text-6xl md:text-7xl leading-[1.1111111] mb-6 font-semibold font-display bg-linear-to-tl from-emerald-500 via-lime-200 from-0% via-30% to-60% to-neutral-300 bg-clip-text text-transparent">Turn endless URLs into flawless links</h1>
+            <h2 className="text-sm xs:text-base sm:text-lg  font-sans md:text-lg dark:text-neutral-300 "><b>No account required</b>. Up to 10 links per session, each <b>active for 30 minutes...</b></h2>
+          </div>
           <Form />
-        </div>
-      </header>
+        </header>
 
-      {/* 👉 En desktop, esta columna derecha será la que scrollea */}
-      <section className="flex-1 lg:h-[510px] lg:py-4 lg:pr-4 lg:overflow-y-auto lg:overflow-x-hidden touch-auto lg:overscroll-contain">
-        <Suspense fallback={<p>Loading...</p>}>
-          <ListURLs initialURLs={urls!} />
-        </Suspense>
-      </section>
-      {/* <header className="lg:max-w-md flex flex-col gap-8">
-        <div className="text-balance [&_h1]:leading-[1.1111111] [&_h1]:mb-6 [&_h2]:text-balance [&_h2]:text-sm/7 md:[&_h2]:text-base/7 ">
-          <h1 className="text-5xl md:text-6xl dark:text-neutral-50">Turn endless URLs into flawless links</h1>
-          <h2 className="dark:text-neutral-300"><b>No account required</b>. Up to 10 links per session, each <b>active for 30 minutes...</b></h2>
-        </div>
-        <div className="sticky z-50 top-0 bg-amber-300">
-
-          <Form />
-        </div>
-      </header>
-
-      <section className="flex-1 lg:py-4 lg:pr-4 lg:overflow-x-hidden lg:overscroll-contain">
-        <Suspense fallback={<p>Loading...</p>}>
-          <ListURLs initialURLs={urls!} />
-        </Suspense>
-      </section> */}
-    </>
+        <section className="w-full">
+          <Suspense fallback={<p>Loading...</p>}>
+            <ShortLinkList initialURLs={urls!} />
+          </Suspense>
+        </section>
+      </div>
+    </div>
   );
 }
