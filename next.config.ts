5ffebddba9d643a0a192ce876af84250db3bd6af
BatDev64 <bat.bits64@gmail.com>
2025-08-25 13:57:11 -0300

    chore: optimize images in production

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+const isVercelProd = process.env.VERCEL_ENV === 'production';
+
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    unoptimized: !(isVercelProd && isProd)
+  }
 };
 
 export default nextConfig;
